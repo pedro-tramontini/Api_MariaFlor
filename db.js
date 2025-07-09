f7551ecb5282ffe760cd8dd4e37f6764bac757f7
Pedro Tramontini <pedrohenriqueyugio@gmail.com>
2025-07-08 21:26:07 -0300

    Versao_Apresentada

--- db.js
+++ db.js
@@ -56,21 +56,66 @@ async function selecionarCliente(){
 
 // ATUALIZAR CLIENTE
 
-    async function atualizarCliente(id, pessoa, cliente, telefone, cidade, endereco){
-        const client = await connect();
-        const insertPessoa = await client.query("UPDATE pessoa SET nome = $1, email = $2 WHERE id = $3 RETURNING id", [pessoa.nome, pessoa.email, id])
+async function atualizarCliente(id, pessoa, cliente, telefone, cidade, endereco) {
+    const client = await connect();
+    
+    try {
+        // Iniciar transação
+        await client.query('BEGIN');
 
-        const insertCidade = await client.query("UPDATE cidade SET nome = $1, uf = $2 WHERE codigo = $3 RETURNING codigo", [cidade.nome, cidade.uf, cidade.codigo])
+        // 1. Atualizar pessoa
+        const pessoaResult = await client.query(
+            "UPDATE pessoa SET nome = $1, email = $2 WHERE id = $3 RETURNING id", 
+            [pessoa.nome, pessoa.email, id]
+        );
+        const idPessoa = pessoaResult.rows[0].id;
 
-        const idPessoa = insertPessoa.rows[0].id
-        const cod_cid = insertCidade.rows[0].codigo
+        // 2. Obter o código da cidade existente
+        const cidadeExistente = await client.query(
+            "SELECT cod_cidade FROM cidade WHERE id_pessoa = $1 LIMIT 1", 
+            [idPessoa]
+        );
+        
+        if (cidadeExistente.rows.length === 0) {
+            throw new Error('Cidade não encontrada para este cliente');
+        }
+        
+        const cod_cid = cidadeExistente.rows[0].cod_cidade;
 
-        await client.query("UPDATE cliente SET cpf= $1 WHERE id_pessoa = $2", [cliente.cpf, idPessoa])
+        // 3. Atualizar cidade
+        await client.query(
+            "UPDATE cidade SET nome = $1, uf = $2 WHERE id_pessoa = $3 AND cod_cidade = $4", 
+            [cidade.nome, cidade.uf, idPessoa, cod_cid]
+        );
 
-        await client.query("UPDATE telefone SET codigo = $1, numero = $2 WHERE id_pessoa = $3", [telefone.codigo, telefone.numero, idPessoa])
+        // 4. Atualizar cliente
+        await client.query(
+            "UPDATE cliente SET cpf = $1 WHERE id_pessoa = $2", 
+            [cliente.cpf, idPessoa]
+        );
 
-        await client.query("UPDATE endereco SET rua = $1, numero = $2, bairro = $3, cep = $4 WHERE id_pessoa = $5 AND cod_cidade = $6", [endereco.rua, endereco.numero, endereco.bairro, endereco.cep, idPessoa,  cod_cid])
+        // 5. Atualizar telefone
+        await client.query(
+            "UPDATE telefone SET codigo = $1, numero = $2 WHERE id_pessoa = $3", 
+            [telefone.codigo, telefone.numero, idPessoa]
+        );
+
+        // 6. Atualizar endereço
+        await client.query(
+            "UPDATE endereco SET rua = $1, numero = $2, bairro = $3, cep = $4 WHERE id_pessoa = $5 AND cod_cidade = $6", 
+            [endereco.rua, endereco.numero, endereco.bairro, endereco.cep, idPessoa, cod_cid]
+        );
+
+        // Confirmar transação
+        await client.query('COMMIT');
+    } catch (error) {
+        // Reverter em caso de erro
+        await client.query('ROLLBACK');
+        throw error;
+    } finally {
+        client.release();
     }
+}
 
 // DELETAR CLIENTE
 
@@ -120,7 +165,7 @@ async function inserirFuncionario(pessoa, funcionario, telefone, cidade, enderec
 
     const idPessoa = insertPessoa.rows[0].id
     const cod_cid = insertCidade.rows[0].codigo
-
+  
     await client.query("INSERT INTO funcionario (id_pessoa, cargo) VALUES ($1, $2)", [idPessoa, funcionario.cargo])
 
     await client.query("INSERT INTO telefone (id_pessoa, numero) VALUES ($1, $2)", [idPessoa, telefone.numero])
@@ -142,22 +187,66 @@ async function selecionarFuncionario(){
 
 // ATUALIZAR FUNCIONÁRIO
 
-    async function atualizarFuncionario(id, pessoa, funcionario, telefone, cidade, endereco){
-        const client = await connect();
-        const insertPessoa = await client.query("UPDATE pessoa SET nome = $1, email = $2 WHERE id = $3 RETURNING id", [pessoa.nome, pessoa.email, id])
+async function atualizarFuncionario(id, pessoa, funcionario, telefone, cidade, endereco) {
+    const client = await connect();
+    
+    try {
+        await client.query('BEGIN'); // Iniciar transação
 
-        const insertCidade = await client.query("UPDATE cidade SET nome = $1, uf = $2 WHERE codigo = $3 RETURNING codigo", [cidade.nome, cidade.uf, cidade.codigo])
+        // 1. Atualizar pessoa e obter id
+        const pessoaResult = await client.query(
+            "UPDATE pessoa SET nome = $1, email = $2 WHERE id = $3 RETURNING id", 
+            [pessoa.nome, pessoa.email, id]
+        );
+        const idPessoa = pessoaResult.rows[0].id;
 
-        const idPessoa = insertPessoa.rows[0].id
-        const cod_cid = insertCidade.rows[0].codigo
+        // 2. Obter o código da cidade associada ao funcionário
+        const cidadeExistente = await client.query(
+            `SELECT c.codigo 
+             FROM cidade c
+             JOIN endereco e ON c.codigo = e.cod_cidade
+             WHERE e.id_pessoa = $1`, 
+            [idPessoa]
+        );
 
-        await client.query("UPDATE funcionario SET cargo= $1 WHERE id_pessoa = $2", [funcionario.cargo, idPessoa])
+        if (cidadeExistente.rows.length === 0) {
+            throw new Error('Não foi encontrada cidade associada a este funcionário');
+        }
 
-        await client.query("UPDATE telefone SET numero = $1 WHERE id_pessoa = $2", [telefone.numero, idPessoa])
+        const cod_cid = cidadeExistente.rows[0].codigo;
 
-        await client.query("UPDATE endereco SET rua = $1, numero = $2, bairro = $3, cep = $4 WHERE id_pessoa = $5 AND cod_cidade = $6", [endereco.rua, endereco.numero, endereco.bairro, endereco.cep, idPessoa,  cod_cid])
+        // 3. Atualizar cidade
+        await client.query(
+            "UPDATE cidade SET nome = $1, uf = $2 WHERE codigo = $3", 
+            [cidade.nome, cidade.uf, cod_cid]
+        );
+
+        // 4. Atualizar funcionario
+        await client.query(
+            "UPDATE funcionario SET cargo = $1 WHERE id_pessoa = $2", 
+            [funcionario.cargo, idPessoa]
+        );
+
+        // 5. Atualizar telefone
+        await client.query(
+            "UPDATE telefone SET numero = $1 WHERE id_pessoa = $2", 
+            [telefone.numero, idPessoa]
+        );
+
+        // 6. Atualizar endereço
+        await client.query(
+            "UPDATE endereco SET rua = $1, numero = $2, bairro = $3, cep = $4 WHERE id_pessoa = $5 AND cod_cidade = $6", 
+            [endereco.rua, endereco.numero, endereco.bairro, endereco.cep, idPessoa, cod_cid]
+        );
+
+        await client.query('COMMIT'); // Confirmar transação
+    } catch (error) {
+        await client.query('ROLLBACK'); // Reverter em caso de erro
+        throw error;
+    } finally {
+        client.release(); // Liberar conexão
     }
-
+}
 // DELETAR FUNCIONÁRIO
 
 async function deletarFuncionario(idPessoa) {
@@ -227,21 +316,68 @@ async function selecionarFornecedor(){
 
 // ATUALIZAR FUNCIONÁRIO
 
-    async function atualizarFornecedor(id, pessoa, fornecedor, telefone, cidade, endereco){
-        const client = await connect();
-        const insertPessoa = await client.query("UPDATE pessoa SET nome = $1, email = $2 WHERE id = $3 RETURNING id", [pessoa.nome, pessoa.email, id])
+async function atualizarFornecedor(id, pessoa, fornecedor, telefone, cidade, endereco) {
+    const client = await connect();
+    
+    try {
+        await client.query('BEGIN'); // Iniciar transação
 
-        const insertCidade = await client.query("UPDATE cidade SET nome = $1, uf = $2 WHERE codigo = $3 RETURNING codigo", [cidade.nome, cidade.uf, cidade.codigo])
+        // 1. Atualizar pessoa e obter id
+        const pessoaResult = await client.query(
+            "UPDATE pessoa SET nome = $1, email = $2 WHERE id = $3 RETURNING id", 
+            [pessoa.nome, pessoa.email, id]
+        );
+        const idPessoa = pessoaResult.rows[0].id;
 
-        const idPessoa = insertPessoa.rows[0].id
-        const cod_cid = insertCidade.rows[0].codigo
+        // 2. Obter o código da cidade associada ao fornecedor
+        const cidadeExistente = await client.query(
+            `SELECT c.codigo 
+             FROM cidade c
+             JOIN endereco e ON c.codigo = e.cod_cidade
+             WHERE e.id_pessoa = $1`, 
+            [idPessoa]
+        );
 
-        await client.query("UPDATE fornecedor SET cnpj= $1 WHERE id_pessoa = $2", [fornecedor.cnpj, idPessoa])
+        if (cidadeExistente.rows.length === 0) {
+            throw new Error('Não foi encontrada cidade associada a este fornecedor');
+        }
 
-        await client.query("UPDATE telefone SET numero = $1 WHERE id_pessoa = $2", [telefone.numero, idPessoa])
+        const cod_cid = cidadeExistente.rows[0].codigo;
 
-        await client.query("UPDATE endereco SET rua = $1, numero = $2, bairro = $3, cep = $4 WHERE id_pessoa = $5 AND cod_cidade = $6", [endereco.rua, endereco.numero, endereco.bairro, endereco.cep, idPessoa,  cod_cid])
+        // 3. Atualizar cidade
+        await client.query(
+            "UPDATE cidade SET nome = $1, uf = $2 WHERE codigo = $3", 
+            [cidade.nome, cidade.uf, cod_cid]
+        );
+
+        // 4. Atualizar fornecedor
+        await client.query(
+            "UPDATE fornecedor SET cnpj = $1 WHERE id_pessoa = $2", 
+            [fornecedor.cnpj, idPessoa]
+        );
+
+        // 5. Atualizar telefone
+        await client.query(
+            "UPDATE telefone SET numero = $1 WHERE id_pessoa = $2", 
+            [telefone.numero, idPessoa]
+        );
+
+        // 6. Atualizar endereço
+        await client.query(
+            "UPDATE endereco SET rua = $1, numero = $2, bairro = $3, cep = $4 WHERE id_pessoa = $5 AND cod_cidade = $6", 
+            [endereco.rua, endereco.numero, endereco.bairro, endereco.cep, idPessoa, cod_cid]
+        );
+
+        await client.query('COMMIT'); // Confirmar transação
+        return { success: true, message: 'Fornecedor atualizado com sucesso' };
+    } catch (error) {
+        await client.query('ROLLBACK'); // Reverter em caso de erro
+        console.error('Erro ao atualizar fornecedor:', error);
+        throw error;
+    } finally {
+        client.release(); // Liberar conexão
     }
+}
 
 // DELETAR FUNCIONÁRIO
 
@@ -270,9 +406,71 @@ async function deletarFornecedor(idPessoa) {
   }
 }
 
+
+
+
+
+// CRUD DE ------------------------------ FORNECEDOR
+// CRUD DE ------------------------------ FORNECEDOR
+// CRUD DE ------------------------------ FORNECEDOR
+// CRUD DE ------------------------------ FORNECEDOR
+// CRIAR PRODUTO
+
+async function inserirProduto(nome, tipo, preco){
+    const client = await connect();
+
+  try {
+    const client = await connect();
+    await client.query("INSERT INTO produto (nome, tipo, preco) VALUES ($1, $2, $3);", [nome, tipo, preco])
+  } finally {
+    await client.release(); // ou client.end() se não for pool
+  }
+  }
+
+// LER PRODUTO
+
+async function selecionarProduto() {
+  const client = await connect();
+    const res = await client.query('SELECT * FROM produto;');
+    return res.rows;
+}
+
+async function selecionarProdutoPorId(id) {
+  const client = await connect();
+  const res = await client.query('SELECT * FROM produto WHERE id = $1;', [id]);
+  return res.rows[0]; // retorna só um produto
+}
+
+// ATUALIZAR FUNCIONÁRIO
+
+    async function atualizarProduto(id, nome, tipo, preco){
+        const client = await connect();
+        await client.query("UPDATE produto SET nome = $1, tipo = $2, preco = $3 WHERE id = $4", [nome, tipo, preco, id])
+    }
+
+// DELETAR FUNCIONÁRIO
+
+async function deletarProduto(id) {
+  const client = await connect();
+
+  try {
+    await client.query('BEGIN');
+
+    // Apaga telefones
+    await client.query('DELETE FROM produto WHERE id = $1;', [id]);
+
+    await client.query('COMMIT');
+  } catch (error) {
+    await client.query('ROLLBACK');
+    throw error;
+  }
+}
+
+
 module.exports = {
     inserirCliente,
     selecionarCliente,
+    selecionarProdutoPorId,
     atualizarCliente,
     deletarCliente,
 
@@ -284,5 +482,11 @@ module.exports = {
     inserirFornecedor,
     selecionarFornecedor,
     atualizarFornecedor,
-    deletarFornecedor
+    deletarFornecedor,
+    
+    inserirProduto,
+    selecionarProduto,
+    atualizarProduto,
+    deletarProduto
+
 }
--- index.js
+++ index.js
@@ -8,40 +8,43 @@ const express = require("express");
 
 const app = express();
 
+
+const cors = require('cors');
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "Funcionando!"
-    })
+  res.json({
+    message: "Funcionando!"
+  })
 })
 
 // ROTAS DO CRUD DE CLIENTE
 
 // CADASTRA CLIENTE
 app.post("/cadastrar-cliente", async (req, res) => {
-    const { pessoa, cliente, telefone, cidade, endereco } = req.body;
-
-    await db.inserirCliente(pessoa, cliente, telefone, cidade, endereco);
-    res.sendStatus(201)
+  const { pessoa, cliente, telefone, cidade, endereco } = req.body;
+  
+  await db.inserirCliente(pessoa, cliente, telefone, cidade, endereco);
+  res.sendStatus(201)
 })
 // LISTA CLIENTE
 app.get("/listar-cliente", async (req, res) => {
-    const pessoa = await db.selecionarCliente();
-    res.json(pessoa);
+  const pessoa = await db.selecionarCliente();
+  res.json(pessoa);
 })
 // ATUALIZA CLIENTE
 app.put("/atualizar-cliente/:id", async (req, res) => {
-    const { id } = req.params;
-    const { pessoa, cliente, telefone, cidade, endereco } = req.body;
-
-    await db.atualizarCliente(parseInt(id), pessoa, cliente, telefone, cidade, endereco);
-    res.json(pessoa);
+  const { id } = req.params;
+  const { pessoa, cliente, telefone, cidade, endereco } = req.body;
+  
+  await db.atualizarCliente(parseInt(id), pessoa, cliente, telefone, cidade, endereco);
+  res.json(pessoa);
 })
 // DELETA CLIENTE
 app.delete("/deletar-cliente/:id", async (req, res) => {
   const { id } = req.params;
-
+  
   try {
     await db.deletarCliente(parseInt(id));
     res.json({ message: `Cliente com id ${id} deletado com sucesso.` });
@@ -66,12 +69,12 @@ app.post("/cadastrar-funcionario", async (req, res) => {
 
     await db.inserirFuncionario(pessoa, funcionario, telefone, cidade, endereco);
     res.sendStatus(201)
-})
-// LISTA FUNCIONARIO
-app.get("/listar-funcionario", async (req, res) => {
+  })
+  // LISTA FUNCIONARIO
+  app.get("/listar-funcionario", async (req, res) => {
     const pessoa = await db.selecionarFuncionario();
     res.json(pessoa);
-})
+  })
 // ATUALIZA FUNCIONARIO
 app.put("/atualizar-funcionario/:id", async (req, res) => {
     const { id } = req.params;
@@ -79,11 +82,11 @@ app.put("/atualizar-funcionario/:id", async (req, res) => {
 
     await db.atualizarFuncionario(parseInt(id), pessoa, funcionario, telefone, cidade, endereco);
     res.json(pessoa);
-})
-// DELETA FUNCIONARIO
-app.delete("/deletar-funcionario/:id", async (req, res) => {
-  const { id } = req.params;
-
+  })
+  // DELETA FUNCIONARIO
+  app.delete("/deletar-funcionario/:id", async (req, res) => {
+    const { id } = req.params;
+    
   try {
     await db.deletarFuncionario(parseInt(id));
     res.json({ message: `Funcionario com id ${id} deletado com sucesso.` });
@@ -103,22 +106,22 @@ app.delete("/deletar-funcionario/:id", async (req, res) => {
 // CADASTRA FORNECEDOR
 app.post("/cadastrar-fornecedor", async (req, res) => {
     const { pessoa, fornecedor, telefone, cidade, endereco } = req.body;
-
+    
     await db.inserirFornecedor(pessoa, fornecedor, telefone, cidade, endereco);
     res.sendStatus(201)
-})
-// LISTA FORNECEDOR
+  })
+  // LISTA FORNECEDOR
 app.get("/listar-fornecedor", async (req, res) => {
     const pessoa = await db.selecionarFornecedor();
     res.json(pessoa);
 })
 // ATUALIZA FORNECEDOR
 app.put("/atualizar-fornecedor/:id", async (req, res) => {
-    const { id } = req.params;
-    const { pessoa, fornecedor, telefone, cidade, endereco } = req.body;
-
-    await db.atualizarFornecedor(parseInt(id), pessoa, fornecedor, telefone, cidade, endereco);
-    res.json(pessoa);
+  const { id } = req.params;
+  const { pessoa, fornecedor, telefone, cidade, endereco } = req.body;
+  
+  await db.atualizarFornecedor(parseInt(id), pessoa, fornecedor, telefone, cidade, endereco);
+  res.json(pessoa);
 })
 // DELETA FORNECEDOR
 app.delete("/deletar-fornecedor/:id", async (req, res) => {
@@ -127,6 +130,62 @@ app.delete("/deletar-fornecedor/:id", async (req, res) => {
   try {
     await db.deletarFornecedor(parseInt(id));
     res.json({ message: `Fornecedor com id ${id} deletado com sucesso.` });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+// ROTAS DO CRUD DE PRODUTO
+
+// CADASTRA PRODUTO
+
+app.post("/cadastrar-produto", async (req, res) => {
+  const { nome, tipo, preco } = req.body;
+  
+  await db.inserirProduto(nome, tipo, preco);
+  res.sendStatus(201)
+})
+// LISTA PRODUTO
+app.get("/listar-produto", async (req, res) => {
+  const produto = await db.selecionarProduto();
+  res.json(produto);
+})
+
+app.get("/listar-produto/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const produto = await db.selecionarProdutoPorId(id);
+
+    if (!produto) {
+      return res.status(404).json({ erro: "Produto não encontrado" });
+    }
+
+    res.json(produto);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Erro ao buscar produto" });
+  }
+});
+
+
+
+// ATUALIZA PRODUTO
+app.put("/atualizar-produto/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, tipo, preco } = req.body;
+  
+  const produto = await db.atualizarProduto(parseInt(id), nome, tipo, preco);
+  res.json(produto);
+})
+// DELETA PRODUTO
+app.delete("/deletar-produto/:id", async (req, res) => {
+  const { id } = req.params;  
+  
+  try {
+    await db.deletarProduto(parseInt(id));
+    res.json({ message: `Produto com id ${id} deletado com sucesso.` });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
